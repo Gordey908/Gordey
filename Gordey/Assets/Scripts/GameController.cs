8216e489a08cdd8e0dd675b0b27036434e014ed8
Gordey908 <144526824+Gordey908@users.noreply.github.com>
2023-10-07 10:53:42 +0200

    Lesson 5

---
+++ Gordey/Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel, gamePanel;
+    public Text startText;
+    [SerializeField] private Text scoreText, bestScoreText;
+    public int score;
+
+    void Start()
+    {
+        score = 0;
+        pausePanel.SetActive(false);
+        gamePanel.SetActive(false);
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    private void BackToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SwitchPause()
+    {
+        pausePanel.SetActive(!pausePanel.activeSelf);
+        gamePanel.SetActive(!gamePanel.activeSelf);
+        switch (Time.timeScale) // Fixed "Time.timescale" to "Time.timeScale"
+        {
+            case 1:
+                Time.timeScale = 0;
+                break;
+            case 0:
+                Time.timeScale = 1;
+                break;
+            default:
+                break;
+        }
+    }
+}
